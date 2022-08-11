bdec37153a5ca6e3995faee7e7f6baf88b498e11
Sorna-sree <110393066+Sorna-sree@users.noreply.github.com>
2022-08-11 20:32:19 +0530

    Add files via upload

---
+++ Budget.py
@@ -0,0 +1,19 @@
+total=int(input("enter the total budget"))
+petrol=3
+Total=total-petrol
+city=input("enter the city")
+if(city=='chennai'):
+    onion=int(input("enter the price"))
+    tomato=int(input("enter the price"))
+    print("onions",total//onion)
+    print("tomato", total//tomato)
+elif(city=='madurai'):
+    onion=int(input("enter the price"))
+    tomato=int(input("enter the price"))
+    print("onions",total//onion)
+    print("tomato",total//tomato)
+elif(city=='thiruchi'):
+    onion=int(input("enter the price"))
+    tomato=int(input("enter the price"))
+    print("onions", total//onion)
+    print("tomato", total//tomato)
